0a446eaa9175e5b12cf3e0cba30219a4c9ff868a
Kha Tran <kha@eatlab.space>
2024-12-29 00:30:11 +0700

    first-commit

---
+++ 584_find_customer_referee.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+
+select name
+from Customer
+where referee_id is null or referee_id != 2;
